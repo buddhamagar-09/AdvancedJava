10efff8c59482bd99b3880d1f2e5b772b64de5f2
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-09-09 19:26:49 +0545

    Swing Demo

--- BasicExamples/FrameDemo.java
+++ BasicExamples/FrameDemo.java
@@ -1,11 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class FrameDemo {
 
     public static void main(String[] args) {
         JFrame f = new JFrame("My first Frame");
-        f.setSize(500, 500);
-        f.setLocation(100, 100);
+        JLabel label = new JLabel("Hello");
+        label.setText("How are you ?");
+        label.setFont(new Font("Zapfino",Font.PLAIN,20));
+        label.setForeground(java.awt.Color.BLUE);
+
+        f.add(label);
+        f.setSize(500, 250);
+        // f.setLocation(100, 100);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
     }
